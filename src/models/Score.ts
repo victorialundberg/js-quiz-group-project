5d1d51de91e89295470beb33255fb8ae44f024dc
Jari Leminaho <jaripiilola@gmail.com>
2024-01-03 10:24:54 +0100

    :building_construction: Adds question array and classes
    Co-authored-by: Victoria Lundberg <victoria.lundbrg@gmail.com>
    Co-authored-by: Caroline Vårsaga <carolinevarsaga@gmail.com>
    Co-authored-by: Ellinor Friström <ellinor.fristrom@utb.ecutbildning.se>
    Co-authored-by: Linda <Zileena6@users.noreply.github.com>

---
+++ src/models/Score.ts
@@ -0,0 +1,60 @@
+/* eslint-disable comma-dangle */
+interface Score {
+  id: number;
+  name: string;
+  //   countDownToPoints: number;
+  totalPoints: number;
+  totalTime: number;
+}
+
+class ScoreItem implements Score {
+  constructor(
+    private _id: number,
+    private _name: string,
+    // private _countDownToPoints: number,
+    private _totalPoints: number,
+    private _totalTime: number
+  ) {}
+
+  get id(): number {
+    return this._id;
+  }
+
+  set id(id: number) {
+    this._id = id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  //   get countDownToPoints(): number {
+  //     return this._countDownToPoints;
+  //   }
+
+  //   set countDownToPoints(countDownToPoints: number) {
+  //     this._countDownToPoints = countDownToPoints;
+  //   }
+
+  get totalPoints(): number {
+    return this._totalPoints;
+  }
+
+  set totalPoints(totalPoints: number) {
+    this._totalPoints = totalPoints;
+  }
+
+  get totalTime(): number {
+    return this._totalTime;
+  }
+
+  set totalTime(totalTime: number) {
+    this._totalTime = totalTime;
+  }
+}
+
+export default ScoreItem;
